f73f5cb958258122f5980db74db7adaf39305d2b
Eu <joaovictorsilvameira@gmail.com>
2025-06-03 15:41:46 -0300

    feat: allow all hosts in Django settings

--- medical_system/settings.py
+++ medical_system/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['18.218.137.47', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
